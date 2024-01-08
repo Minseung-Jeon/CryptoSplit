2cf35c75e5b49996c13afe942e41d9a00a6e64dc
Minseung <tonyjun00@gmail.com>
2024-01-08 17:59:50 -0500

    version 1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "FALSE") == "True"
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "tonyjun00.pythonanywhere.com"]
 
 
 # Application definition
