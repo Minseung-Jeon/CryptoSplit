d33fc18714bda1321f1675ff751c2e57e653b0dd
Minseung <tonyjun00@gmail.com>
2024-01-08 19:35:20 -0500

    final without email sepecification

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "tonyjun00.pythonanywhere.com"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
